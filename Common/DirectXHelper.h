2e3db9617c4a8cc3640d33683f2678977a6b95cc
hot3dx <hot3dx@hotmail.com>
2021-02-27 19:03:07 -0700

    Add files via upload

---
+++ Common/DirectXHelper.h
@@ -0,0 +1,232 @@
+﻿#pragma once
+
+#include "pch.h"
+#include <ppltasks.h>	// For create_task
+#include <strsafe.h>
+
+typedef unsigned char mybyte;
+
+namespace DX
+{
+    static const UINT c_frameCount = 3;		// Use triple buffering.
+    static const UINT NumContexts = 3;
+    static const UINT NumLights = 3;        // Keep this in sync with "shaders.hlsl".
+
+    static const UINT TitleThrottle = 200;    // Only update the titlebar every X number of frames.
+
+    // Command list submissions from main thread.
+    static const int CommandListCount = 3;
+    static const int CommandListPre = 0;
+    static const int CommandListMid = 1;
+    static const int CommandListPost = 2;
+
+	inline void ThrowIfFailed(HRESULT hr)
+	{
+		if (FAILED(hr))
+		{
+			// Set a breakpoint on this line to catch Win32 API errors.
+  			throw Platform::Exception::CreateException(hr);
+		}
+	}
+
+	// Function that reads from a binary file asynchronously.
+	inline Concurrency::task<std::vector<mybyte>> ReadDataAsync(const std::wstring& filename)
+	{
+		using namespace Windows::Storage;
+		using namespace Concurrency;
+
+		auto folder = Windows::ApplicationModel::Package::Current->InstalledLocation;
+
+		return create_task(folder->GetFileAsync(Platform::StringReference(filename.c_str()))).then([](StorageFile^ file)
+		{
+			return FileIO::ReadBufferAsync(file);
+		}).then([](Streams::IBuffer^ fileBuffer) -> std::vector<mybyte>
+		{
+			std::vector<mybyte> returnBuffer;
+			returnBuffer.resize(fileBuffer->Length);
+			Streams::DataReader::FromBuffer(fileBuffer)->ReadBytes(Platform::ArrayReference<mybyte>(returnBuffer.data(), fileBuffer->Length));
+			return returnBuffer;
+		});
+	}
+
+    
+	// Converts a length in device-independent pixels (DIPs) to a length in physical pixels.
+	inline float ConvertDipsToPixels(float dips, float dpi)
+	{
+		static const float dipsPerInch = 96.0f;
+		return floorf(dips * dpi / dipsPerInch + 0.5f); // Round to nearest integer.
+	}
+
+    // Assign a name to the object to aid with debugging.
+#if defined(_DEBUG) || defined(DBG)
+    inline void SetName(ID3D12Object* pObject, LPCWSTR name)
+    {
+        pObject->SetName(name);
+    }
+    inline void SetNameIndexed(ID3D12Object* pObject, LPCWSTR name, UINT index)
+    {
+        WCHAR fullName[50];
+        StringCbPrintf(fullName, 50, L"%s[%u]", name, index);
+        if (fullName > 0)
+        {
+            pObject->SetName(fullName);
+        }
+}
+#else
+    inline void SetName(ID3D12Object*, LPCWSTR)
+    {
+    }
+    inline void SetNameIndexed(ID3D12Object*, LPCWSTR, UINT)
+    {
+    }
+#endif
+
+    // Naming helper for ComPtr<T>.
+    // Assigns the name of the variable as the name of the object.
+    // The indexed variant will include the index in the name of the object.
+#define NAME_D3D12_OBJECT(x) SetName((x).Get(), L#x)
+#define NAME_D3D12_OBJECT_INDEXED(x, n) SetNameIndexed((x)[n].Get(), L#x, n)
+
+	/*
+#if defined(_DEBUG)
+	// Check for SDK Layer support.
+	inline bool SdkLayersAvailable()
+	{
+		HRESULT hr = D3D12CreateDevice(
+			nullptr,
+			D3D_DRIVER_TYPE_NULL,       // There is no need to create a real hardware device.
+			0,
+			D3D11_CREATE_DEVICE_DEBUG,  // Check for the SDK layers.
+			nullptr,                    // Any feature level will do.
+			0,
+			D3D11_SDK_VERSION,          // Always set this to D3D11_SDK_VERSION for Microsoft Store apps.
+			nullptr,                    // No need to keep the D3D device reference.
+			nullptr,                    // No need to know the feature level.
+			nullptr                     // No need to keep the D3D device context reference.
+			);
+
+		return SUCCEEDED(hr);
+	}
+#endif
+*/
+}
+
+#define SAFE_RELEASE(p) if (p) (p)->Release()
+
+inline void GetAssetsPath(_Out_writes_(pathSize) WCHAR* path, UINT pathSize)
+{
+    if (path == nullptr)
+    {
+        throw std::exception();
+    }
+
+    DWORD size = GetModuleFileName(nullptr, path, pathSize);
+    if (size == 0 || size == pathSize)
+    {
+        // Method failed or path was truncated.
+        throw std::exception();
+    }
+
+    WCHAR* lastSlash = wcsrchr(path, L'\\');
+    if (lastSlash)
+    {
+        *(lastSlash + 1) = L'\0';
+    }
+}
+
+inline HRESULT ReadDataFromFile(LPCWSTR filename, mybyte** data, UINT* size)
+{
+    using namespace Microsoft::WRL;
+
+    CREATEFILE2_EXTENDED_PARAMETERS extendedParams = {};
+    extendedParams.dwSize = sizeof(CREATEFILE2_EXTENDED_PARAMETERS);
+    extendedParams.dwFileAttributes = FILE_ATTRIBUTE_NORMAL;
+    extendedParams.dwFileFlags = FILE_FLAG_SEQUENTIAL_SCAN;
+    extendedParams.dwSecurityQosFlags = SECURITY_ANONYMOUS;
+    extendedParams.lpSecurityAttributes = nullptr;
+    extendedParams.hTemplateFile = nullptr;
+
+    Wrappers::FileHandle file(CreateFile2(filename, GENERIC_READ, FILE_SHARE_READ, OPEN_EXISTING, &extendedParams));
+    if (file.Get() == INVALID_HANDLE_VALUE)
+    {
+        throw std::exception();
+    }
+
+    FILE_STANDARD_INFO fileInfo = {};
+    if (!GetFileInformationByHandleEx(file.Get(), FileStandardInfo, &fileInfo, sizeof(fileInfo)))
+    {
+        throw std::exception();
+    }
+
+    if (fileInfo.EndOfFile.HighPart != 0)
+    {
+        throw std::exception();
+    }
+
+    *data = reinterpret_cast<mybyte*>(malloc(fileInfo.EndOfFile.LowPart));
+    *size = fileInfo.EndOfFile.LowPart;
+
+    if (!ReadFile(file.Get(), *data, fileInfo.EndOfFile.LowPart, nullptr, nullptr))
+    {
+        throw std::exception();
+    }
+
+    return S_OK;
+}
+
+
+inline UINT CalculateConstantBufferByteSize(UINT byteSize)
+{
+    // Constant buffer size is required to be aligned.
+    return (byteSize + (D3D12_CONSTANT_BUFFER_DATA_PLACEMENT_ALIGNMENT - 1)) & ~(D3D12_CONSTANT_BUFFER_DATA_PLACEMENT_ALIGNMENT - 1);
+}
+
+#ifdef D3D_COMPILE_STANDARD_FILE_INCLUDE
+inline Microsoft::WRL::ComPtr<ID3DBlob> CompileShader(
+    const std::wstring& filename,
+    const D3D_SHADER_MACRO* defines,
+    const std::string& entrypoint,
+    const std::string& target)
+{
+    UINT compileFlags = 0;
+#if defined(_DEBUG) || defined(DBG)
+    compileFlags = D3DCOMPILE_DEBUG | D3DCOMPILE_SKIP_OPTIMIZATION;
+#endif
+
+    HRESULT hr;
+
+    Microsoft::WRL::ComPtr<ID3DBlob> byteCode = nullptr;
+    Microsoft::WRL::ComPtr<ID3DBlob> errors;
+    hr = D3DCompileFromFile(filename.c_str(), defines, D3D_COMPILE_STANDARD_FILE_INCLUDE,
+        entrypoint.c_str(), target.c_str(), compileFlags, 0, &byteCode, &errors);
+
+    if (errors != nullptr)
+    {
+        OutputDebugStringA((char*)errors->GetBufferPointer());
+    }
+    ThrowIfFailed(hr);
+
+    return byteCode;
+}
+#endif
+
+// Resets all elements in a ComPtr array.
+template<class T>
+void ResetComPtrArray(T* comPtrArray)
+{
+    for (auto& i : *comPtrArray)
+    {
+        i.Reset();
+    }
+}
+
+
+// Resets all elements in a unique_ptr array.
+template<class T>
+void ResetUniquePtrArray(T* uniquePtrArray)
+{
+    for (auto& i : *uniquePtrArray)
+    {
+        i.reset();
+    }
+}
