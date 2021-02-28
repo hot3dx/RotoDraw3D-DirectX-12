8ef3ffdba04d0c0d37c0af78b632a2e00c99cf85
hot3dx <hot3dx@hotmail.com>
2021-02-27 18:56:15 -0700

    Add files via upload

---
+++ App.xaml.cpp
@@ -0,0 +1,185 @@
+//--------------------------------------------------------------------------------------
+// File: 
+//
+// Copyright (c) Jeff Kubitz - hot3dx. All rights reserved.
+// 
+// No warranty is expressed or implied use at own risk
+//
+//--------------------------------------------------------------------------------------
+//
+// File: App.xaml.cpp
+// Implementation of the App class.
+//
+
+#include "pch.h"
+#include "App.xaml.h"
+#include "DirectXPage.xaml.h"
+#include "Hot3dxRotoDrawMain.h"
+
+using namespace Hot3dxRotoDraw;
+
+using namespace Platform;
+using namespace Windows::ApplicationModel;
+using namespace Windows::ApplicationModel::Activation;
+using namespace Windows::Foundation;
+using namespace Windows::Foundation::Collections;
+using namespace Windows::Storage;
+using namespace Windows::UI::Xaml;
+using namespace Windows::UI::Xaml::Controls;
+using namespace Windows::UI::Xaml::Controls::Primitives;
+using namespace Windows::UI::Xaml::Data;
+using namespace Windows::UI::Xaml::Input;
+using namespace Windows::UI::Xaml::Interop;
+using namespace Windows::UI::Xaml::Media;
+using namespace Windows::UI::Xaml::Navigation;
+
+// These placeholder functions are used if the sample does not
+// implement the corresponding methods. This allows us to simulate
+// C# partial methods in C++.
+
+static void Partial_Construct() { }
+static void Partial_LaunchCompleted(LaunchActivatedEventArgs ^ e) { }
+
+/// <summary>
+/// Initializes the singleton application object.  This is the first line of authored code
+/// executed, and as such is the logical equivalent of main() or WinMain().
+/// </summary>
+App::App()
+{
+	InitializeComponent();
+	Suspending += ref new SuspendingEventHandler(this, &App::OnSuspending);
+	Resuming += ref new EventHandler<Object^>(this, &App::OnResuming);
+
+#if defined(_DEBUG)
+	UnhandledException += ref new UnhandledExceptionEventHandler([](Object^ /* sender */, UnhandledExceptionEventArgs^ args)
+		{
+			String^ error = "Hot3dxGraphicsXaml::App::UnhandledException: " + args->Message + "\n";
+			OutputDebugStringW(error->Data());
+		});
+#endif
+}
+
+/// <summary>
+/// Invoked when the application is launched normally by the end user.  Other entry points
+/// will be used when the application is launched to open a specific file, to display
+/// search results, and so forth.
+/// </summary>
+/// <param name="e">Details about the launch request and process.</param>
+void App::OnLaunched(Windows::ApplicationModel::Activation::LaunchActivatedEventArgs^ e)
+{
+#if _DEBUG
+	if (IsDebuggerPresent())
+	{
+		DebugSettings->EnableFrameRateCounter = true;
+	}
+#endif
+
+	auto rootFrame = dynamic_cast<Frame^>(Window::Current->Content);
+
+	// Do not repeat app initialization when the Window already has content,
+	// just ensure that the window is active
+	if (rootFrame == nullptr)
+	{
+		// Create a Frame to act as the navigation context and associate it with
+		// a SuspensionManager key
+		rootFrame = ref new Frame();
+
+		rootFrame->NavigationFailed += ref new Windows::UI::Xaml::Navigation::NavigationFailedEventHandler(this, &App::OnNavigationFailed);
+
+		// Place the frame in the current Window
+		Window::Current->Content = rootFrame;
+	}
+
+	if (rootFrame->Content == nullptr)
+	{
+		// When the navigation stack isn't restored navigate to the first page,
+		// configuring the new page by passing required information as a navigation
+		// parameter
+		rootFrame->Navigate(TypeName(DirectXPage::typeid), e->Arguments);
+	}
+
+	if (m_directXPage == nullptr)
+	{
+		m_directXPage = dynamic_cast<DirectXPage^>(rootFrame->Content);
+	}
+
+	if (e->PreviousExecutionState == ApplicationExecutionState::Terminated)
+	{
+		m_directXPage->LoadInternalState(ApplicationData::Current->LocalSettings->Values);
+	}
+	
+	// Ensure the current window is active
+	Window::Current->Activate();
+}
+/// <summary>
+/// Invoked when application execution is being suspended.  Application state is saved
+/// without knowing whether the application will be terminated or resumed with the contents
+/// of memory still intact.
+/// </summary>
+/// <param name="sender">The source of the suspend request.</param>
+/// <param name="e">Details about the suspend request.</param>
+void App::OnSuspending(Object^ sender, SuspendingEventArgs^ e)
+{
+	(void) sender;	// Unused parameter
+	(void) e;	// Unused parameter
+
+	m_directXPage->SaveInternalState(ApplicationData::Current->LocalSettings->Values);
+}
+
+/// <summary>
+/// Invoked when application execution is being resumed.
+/// </summary>
+/// <param name="sender">The source of the resume request.</param>
+/// <param name="args">Details about the resume request.</param>
+void App::OnResuming(Object ^sender, Object ^args)
+{
+	(void) sender; // Unused parameter
+	(void) args; // Unused parameter
+
+	m_directXPage->LoadInternalState(ApplicationData::Current->LocalSettings->Values);
+}
+
+/// <summary>
+/// Invoked when Navigation to a certain page fails
+/// </summary>
+/// <param name="sender">The Frame which failed navigation</param>
+/// <param name="e">Details about the navigation failure</param>
+void App::OnNavigationFailed(Platform::Object ^sender, Windows::UI::Xaml::Navigation::NavigationFailedEventArgs ^e)
+{
+	throw ref new FailureException("Failed to load Page " + e->SourcePageType.Name);
+}
+
+
+// DisplayInformation event handlers.
+// ReAdded 3/9/19 10:32 am
+std::shared_ptr<DX::DeviceResources> App::GetDeviceResources()
+{
+	if (m_deviceResources != nullptr && m_deviceResources->IsDeviceRemoved())
+	{
+		// All references to the existing D3D device must be released before a new device
+		// can be created.
+
+		m_deviceResources = nullptr;
+		// doesn't do anything
+		//m_main->OnDeviceRemoved();
+
+#if defined(_DEBUG)
+		/*
+		ComPtr<IDXGIDebug1> dxgiDebug;
+		if (SUCCEEDED(DXGIGetDebugInterface1(0, IID_PPV_ARGS(&dxgiDebug))))
+		{
+			dxgiDebug->ReportLiveObjects(DXGI_DEBUG_ALL, DXGI_DEBUG_RLO_FLAGS(DXGI_DEBUG_RLO_SUMMARY | DXGI_DEBUG_RLO_IGNORE_INTERNAL));
+		}
+		*/
+#endif
+	}
+
+	if (m_deviceResources == nullptr)
+	{
+		m_deviceResources = std::make_shared<DX::DeviceResources>();
+		m_deviceResources->SetCoreWindow(Windows::UI::Core::CoreWindow::GetForCurrentThread());
+		// resets TouchMap and do I even need it?
+		//m_main->CreateRenderers(m_deviceResources);
+	}
+	return m_deviceResources;
+}
---
+++ App.xaml.h
@@ -0,0 +1,47 @@
+//--------------------------------------------------------------------------------------
+// File: 
+//
+// Copyright (c) Jeff Kubitz - hot3dx. All rights reserved.
+// 
+// No warranty is expressed or implied use at own risk
+//
+//--------------------------------------------------------------------------------------
+//
+// File: App.xaml.h
+// Declaration of the App class.
+//
+
+#pragma once
+
+#include "App.g.h"
+#include "DirectXPage.xaml.h"
+#include "Common\DeviceResources.h"
+#include "Hot3dxRotoDrawMain.h"
+
+using namespace DX;
+
+namespace Hot3dxRotoDraw
+{
+		/// <summary>
+	/// Provides application-specific behavior to supplement the default Application class.
+	/// </summary>
+	ref class App sealed
+	{
+	public:
+		App();
+		virtual void OnLaunched(Windows::ApplicationModel::Activation::LaunchActivatedEventArgs^ e) override;
+
+	private:
+		void OnSuspending(Platform::Object^ sender, Windows::ApplicationModel::SuspendingEventArgs^ e);
+		void OnResuming(Platform::Object ^sender, Platform::Object ^args);
+		void OnNavigationFailed(Platform::Object ^sender, Windows::UI::Xaml::Navigation::NavigationFailedEventArgs ^e);
+		DirectXPage^ m_directXPage;
+
+		// Private accessor for m_deviceResources, protects against device removed errors.
+		std::shared_ptr<DX::DeviceResources> GetDeviceResources();
+
+
+		std::shared_ptr<DX::DeviceResources> m_deviceResources;
+		std::unique_ptr<Hot3dxRotoDrawMain> m_main;
+	};
+}
